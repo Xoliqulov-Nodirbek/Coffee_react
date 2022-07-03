0fe90e7ef8f6f57c7acf4b34cda67edbdfabf3c9
Xoliqulov-Nodirbek <xoliqulov.nodirbek2003@gmail.com>
2022-07-03 14:05:16 +0500

    removed warnings

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -6,9 +6,9 @@ const Nav = () => {
     return (
         <nav className="header__nav">
             <ul className="header__nav--list">
-                <li className="header__nav--item"><a className="header__nav--link" href="#">HOME</a></li>
-                <li className="header__nav--item"><a className="header__nav--link" href="#">ABOUT US</a></li>
-                <li className="header__nav--item"><a className="header__nav--link" href="#">CREATE YOUR PLAN</a></li>
+                <li className="header__nav--item"><a className="header__nav--link" href="https://google.com">HOME</a></li>
+                <li className="header__nav--item"><a className="header__nav--link" href="https://google.com">ABOUT US</a></li>
+                <li className="header__nav--item"><a className="header__nav--link" href="https://google.com">CREATE YOUR PLAN</a></li>
             </ul>
         </nav>
     )
